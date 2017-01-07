b293c115fa411a97283bf743c3cf49a4a37a614d
bobsoldier <2763547660@qq.com>
2017-01-07 14:23:32 +0800

    init simple datastructures chapter8

---
+++ DataStructuresAndAlgorithms/SimpleDataStructure/code/inc/chapter8.h
@@ -0,0 +1,11 @@
+#ifndef __SIMPLEDATASTRUCTURES_CHAPTER_8__
+#define __SIMPLEDATASTRUCTURES_CHAPTER_8__
+
+
+namespace SimpleDataStructures
+{
+/* 顺序查找 a为数组 n为数组的元素个数 key为要超找的值*/
+static int Sequential_Search(int* a, int n, int key);
+}
+
+#endif
---
+++ DataStructuresAndAlgorithms/SimpleDataStructure/code/src/chapter8.cpp
@@ -0,0 +1,21 @@
+#include "chapter8.h"
+
+namespace SimpleDataStructures
+{
+	
+int Sequential_Search(int* a, int n, int key)
+{
+	if(NULL == a) {
+		return -1;
+	}
+	
+	for(int i = 0; i < n; ++i) {
+		if(*(a+i) == key) {
+			return i;
+		}
+	}
+	
+	return -1;
+}
+
+}
