1eb6d8b72b1d9e047947923a97f27012b666ea5b
bobsoldier <2763547660@qq.com>
2017-01-20 11:09:15 +0800

    Binary Tree

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main()
 {
 // 	SimpleDataStructuresTest::Chapter8UnitTest::Sequential_Search_Unit_Test();
 // 	SimpleDataStructuresTest::Chapter8UnitTest::Binary_Search_Unit_test();
-// 	SimpleDataStructuresTest::Chapter8UnitTest::Binary_Sort_Tree_Unit_Test();
+ 	SimpleDataStructuresTest::Chapter8UnitTest::Binary_Sort_Tree_Unit_Test();
 
-	SimpleDataStructuresTest::Chapter6UnitTest::PreOrderTraverse_Unit_Test();
+//	SimpleDataStructuresTest::Chapter6UnitTest::PreOrderTraverse_Unit_Test();
 
 	system("Pause"); 
 
