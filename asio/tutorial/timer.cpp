86aebfe434a8da08dbc7141514ee74cddd45a557
shibaobao <shibaobao@vilgame.com>
2018-10-23 11:20:20 +0800

    ...

--- asio/tutorial/timer.cpp
+++ asio/tutorial/timer.cpp
@@ -2,7 +2,7 @@
 #include <boost/asio.hpp>
 
 int main(int argc, char* argv[]) {
-    boost::asio::io_context io;
+    boost::asio::io_service io;
     boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
     t.wait();
     std::cout << "hello world!\n";
